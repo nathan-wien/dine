d62809e4cd8e07c124bfd260c82b85433bbca7f3
nathan-wien <nhat.nguyen.cs17@gmail.com>
2022-04-23 20:31:36 +1000

    doc(rst): try to make rtd work

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-# autodoc_mock_imports = ["dine"]
+autodoc_mock_imports = ["dine"]
 
 
 # -- Project information -----------------------------------------------------
